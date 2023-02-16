28ccaceed49b95cfc7a7e88e6a5ac69d23bd6f42
oTalDoHud <hudson.lucas.vieira@gmail.com>
2023-02-16 17:32:14 -0300

    Projeto criado

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+main() => runApp(ControleGastosApp());
+
+class ControleGastosApp extends StatelessWidget {
+  const ControleGastosApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: MyHomeApp(),
+    );
+  }
+}
+
+class MyHomeApp extends StatelessWidget {
+  const MyHomeApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Despesas Pessoais"),
+      ),
+      body: const Center(
+        child: Text("Versão inicial"),
+      ),
+    );
+  }
+}
