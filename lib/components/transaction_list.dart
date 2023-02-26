a0494abf537c4023c0aba13b9d03d7644553a094
oTalDoHud <hudson.lucas.vieira@gmail.com>
2023-02-26 19:36:50 -0300

    Utilizando ListTile

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -35,45 +35,32 @@ class TransactionList extends StatelessWidget {
               itemBuilder: (BuildContext ctx, int index) {
                 final tr = _transaction[index];
                 return Card(
-                  child: Row(
-                    children: <Widget>[
-                      Container(
-                        margin: const EdgeInsets.symmetric(
-                          horizontal: 15,
-                          vertical: 10,
-                        ),
-                        decoration: BoxDecoration(
-                            border: Border.all(
-                          color: Theme.of(context).colorScheme.primary,
-                          width: 2,
-                        )),
-                        padding: const EdgeInsets.all(10),
-                        child: Text(
-                          "R\$: ${tr.value.toStringAsFixed(2)}",
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
-                            color: Theme.of(context).colorScheme.primary,
+                  margin: const EdgeInsets.symmetric(
+                    vertical: 8,
+                    horizontal: 5,
+                  ),
+                  elevation: 5,
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      backgroundColor: Theme.of(context).colorScheme.primary,
+                      radius: 35,
+                      child: Padding(
+                        padding: const EdgeInsets.all(12.0),
+                        child: FittedBox(
+                          child: Text(
+                            style: const TextStyle(color: Colors.white),
+                            "R\$${tr.value}",
                           ),
                         ),
                       ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            tr.title,
-                            style: Theme.of(context).textTheme.titleLarge,
-                          ),
-                          Text(
-                            DateFormat("d MMM y").format(tr.date),
-                            style: const TextStyle(
-                              color: Colors.grey,
-                              fontSize: 16,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ],
+                    ),
+                    title: Text(
+                      tr.title,
+                      style: Theme.of(context).textTheme.titleLarge,
+                    ),
+                    subtitle: Text(
+                      DateFormat("d MMM y").format(tr.date),
+                    ),
                   ),
                 );
               },
